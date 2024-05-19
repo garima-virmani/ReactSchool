0283e3f30cad3398f6b8da94f4bbd75e0bedf3bc
garima-racloop <garima@racloop.com>
2024-05-19 23:17:08 +0530

    hooks-implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useCallback, useMemo, useState } from "react";
 
 function App() {
+  // declaring state variable
+  const [count, setCount] = useState(0);
+
+  const otp = 30_000_000_0;
+  const arr = useMemo(() => {
+   return Array(otp).map(() => {});
+  },[]);
+
+  // fn memoising
+  useCallback(() => {
+    for (let i = 0; i < otp; i++) {}
+  },[]);
+
   return (
     <>
       <div style={{ marginLeft: "40%", marginTop: "10%" }}>
-        <h2>Value: 0</h2>
+        <h2>Value: {count}</h2>
 
         <div
           style={{
@@ -14,8 +28,8 @@ function App() {
             justifyContent: "space-between",
           }}
         >
-          <button>Increment</button>
-          <button>Decrement</button>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
         </div>
       </div>
     </>
